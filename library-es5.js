7b0c110b414361e910ada8f3f38e1db50165bb63
Yury Zimnitski <demovie.inc@live.com>
2021-05-18 23:04:24 +0300

    Rework library-es5. Now chain function incapsulated

--- library-es5.js
+++ library-es5.js
@@ -1,104 +1,92 @@
-let arrayExtensionsES5 = {
-  array: [],
-  chain(inputArray) {
-    this.array = inputArray.slice();
-    return this;
+var arrayExtensionsES5 = {
+  chain(array) {
+    return {
+      take(n) {
+        array = arrayExtensionsES5.take(array, n);
+        return this;
+      },
+      skip(n) {
+        array = arrayExtensionsES5.skip(array, n);
+        return this;
+      },
+      map(callback) {
+        array = arrayExtensionsES5.map(array, callback);
+        return this;
+      },
+      reduce(callback, initialValue) {
+        return arrayExtensionsES5.reduce(array, callback, initialValue);
+      },
+      filter(callback) {
+        return arrayExtensionsES5.filter(array, callback);
+      },
+
+      foreach(callback) {
+        array = arrayExtensionsES5.foreach(array, callback);
+        return this;
+      },
+      value() {
+        return array;
+      },
+    };
   },
 
-  take(n) {
-    this.array = take(this.array, n);
-    return this;
-  },
+  take(array, n) {
+    var tempArray = [];
 
-  skip(n) {
-    this.array = skip(this.array, n);
-    return this;
-  },
+    if (n > 0) {
+      for (let i = 0; i < array.length; i++) {
+        if (i == n) {
+          break;
+        }
+        tempArray.push(array[i]);
+      }
+    }
 
-  map(callback) {
-    this.array = map(this.array, callback);
-    return this;
+    return tempArray;
   },
+  skip(array, n) {
+    var tempArray = [];
 
-  reduce(callback, initialValue) {
-    return reduce(this.array, callback, initialValue);
+    if (n < array.length) {
+      for (let i = n; i < array.length; i++) {
+        tempArray.push(array[i]);
+      }
+    }
+
+    return tempArray;
   },
+  map(array, callback) {
+    var tempArray = [];
+    for (let i = 0; i < array.length; i++) {
+      tempArray[i] = callback(array[i]);
+    }
 
-  filter(callback) {
-    return filter(this.array, callback);
+    return tempArray;
   },
+  reduce(array, callback, initialValue) {
+    // if initialValaue undefined it will get the value 0
+    initialValue = initialValue || 0;
 
-  foreach(callback) {
-    this.array = foreach(this.array, callback);
-    return this;
+    for (let i = 0; i < array.length; i++) {
+      initialValue = callback(initialValue, array[i]);
+    }
+
+    return initialValue;
   },
+  filter(array, callback) {
+    var tempArray = [];
 
-  value() {
-    return this.array;
+    for (let i = 0; i < array.length; i++) {
+      if (callback(array[i])) {
+        tempArray.push(array[i]);
+      }
+    }
+
+    return tempArray;
+  },
+  foreach(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+      callback(array[i]);
+    }
   },
 };
-
-function take(array, n) {
-  let tempArray = [];
-
-  if (n > 0) {
-    for (let i = 0; i < array.length; i++) {
-      if (i == n) {
-        break;
-      }
-      tempArray.push(array[i]);
-    }
-  }
-
-  return tempArray;
-}
-
-function skip(array, n) {
-  let tempArray = [];
-
-  if (n < array.length) {
-    for (let i = n; i < array.length; i++) {
-      tempArray.push(array[i]);
-    }
-  }
-
-  return tempArray;
-}
-
-function map(array, callback) {
-  let tempArray = [];
-  for (let i = 0; i < array.length; i++) {
-    tempArray[i] = callback(array[i]);
-  }
-
-  return tempArray;
-}
-
-function reduce(array, callback, initialValue) {
-  // if initialValaue undefined it will get the value 0
-  initialValue = initialValue || 0;
-
-  for (let i = 0; i < array.length; i++) {
-    initialValue = callback(initialValue, array[i]);
-  }
-
-  return initialValue;
-}
-
-function filter(array, callback) {
-  let tempArray = [];
-
-  for (let i = 0; i < array.length; i++) {
-    if (callback(array[i])) {
-      tempArray.push(array[i]);
-    }
-  }
-
-  return tempArray;
-}
-
-function foreach(array, callback) {
-  for (let i = 0; i < array.length; i++) {
-    callback(array[i]);
-  }
-}
